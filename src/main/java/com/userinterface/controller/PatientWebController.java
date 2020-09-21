2d1741b987a628f29a4ed999544b25913348d3e1
roshan340 <roshanbanarasi@gmail.com>
2020-09-21 11:51:51 +0000

    roshan updated

--- src/main/java/com/userinterface/controller/PatientWebController.java
+++ src/main/java/com/userinterface/controller/PatientWebController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -35,7 +36,25 @@ public class PatientWebController {
 	        return "patient/list";
 	        
 	    }
-	    
-	    
+	    @RequestMapping({ "/patient/new", "/patient" })
+	    public String newPatient(Model model) {
+	   
+	        System.out.println("Submit form 1");
+	        System.out.println(patientService.getById("14").getFirstname());
+	        model.addAttribute("patients", patientService.listAll());
+	        System.out.println("Submit form 2");
+	        System.out.println(patientService.getById("14").getDob());
+	        return "patient/patientform";
+	        
+	    }
+	    @RequestMapping("/patient/show/{id}")
+    public String getPatient(@PathVariable String id, Model model) {
+        Patient p = patientService.getById(id);
+        //model.addAttribute("age",Period.between(patientService.getById(id).getDob(), LocalDate.now()).getYears());
+        model.addAttribute("patient", p);
+        //Patient xyz = patientService.getById(id);
+        //System.out.println(Period.between(patientService.getById(id).getDob(), LocalDate.now()).getYears());
+        return "patient/show";
+    }
 	    
 }
